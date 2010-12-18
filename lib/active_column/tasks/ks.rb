b1c6895596b271a69f71abf523c2ae03cbc94a17
Michael Wynholds <mike@carbonfive.com>
2010-12-17 17:08:01 -0800

    Add drop and drop:all tasks.

--- lib/active_column/tasks/ks.rb
+++ lib/active_column/tasks/ks.rb
@@ -4,16 +4,34 @@ namespace :ks do
 
   desc 'Create the keyspace in config/cassandra.yml for the current environment'
   task :create => :environment do
-    config = YAML.load_file(Rails.root.join("config", "cassandra.yml"))[Rails.env]
+    config = load_config[Rails.env]
     ActiveColumn::Tasks::Keyspace.new.create config[:keyspace], config
   end
 
-  desc 'Create keyspaces in config/cassandra.yml for development and test environments'
+  desc 'Create keyspaces in config/cassandra.yml for all environments'
   task 'create:all' => :environment do
-    config = YAML.load_file(Rails.root.join("config", "cassandra.yml"))
-    [:development, :test].each do |env|
+    config = load_config
+    config.keys.each do |env|
       ActiveColumn::Tasks::Keyspace.new.create config[env][:keyspace], config
     end
   end
 
+  desc 'Drop keyspace in config/cassandra.yml for the current environment'
+  task :drop => :environment do
+    config = load_config[Rails.env]
+    ActiveColumn::Tasks::Keyspace.new.drop config[:keyspace]
+  end
+
+  desc 'Drop keyspaces in config/cassandra.yml for all environments'
+  task 'deop:all' => :environment do
+    config = load_config
+    config.keys.each do |env|
+      ActiveColumn::Tasks::Keyspace.new.drop config[env][:keyspace], config
+    end
+  end
+
+end
+
+def load_config
+  YAML.load_file(Rails.root.join("config", "cassandra.yml"))
 end
