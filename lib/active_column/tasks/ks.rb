d323b445cf61dd4f1b3f8eed51220624d000d1ce
Michael Wynholds <mike@carbonfive.com>
2010-12-21 11:43:59 -0800

    Add some ks: tasks.

--- lib/active_column/tasks/ks.rb
+++ lib/active_column/tasks/ks.rb
@@ -23,15 +23,44 @@ namespace :ks do
   end
 
   desc 'Drop keyspaces in config/cassandra.yml for all environments'
-  task 'deop:all' => :environment do
+  task 'drop:all' => :environment do
     config = load_config
     config.keys.each do |env|
       ActiveColumn::Tasks::Keyspace.new.drop config[env][:keyspace], config
     end
   end
 
+  desc 'Migrate the keyspace (options: VERSION=x)'
+  task :migrate => :environment do
+    set_keyspace
+    version = ( ENV['VERSION'] ? ENV['VERSION'].to_i : nil )
+    ActiveColumn::Migrator.migrate ActiveColumn::Migrator.migrations_path, version
+  end
+
+  desc 'Rolls the schema back to the previous version (specify steps w/ STEP=n)'
+  task :rollback => :environment do
+    set_keyspace
+    step = ENV['STEP'] ? ENV['STEP'].to_i : 1
+    ActiveColumn::Migrator.rollback ActiveColumn::Migrator.migrations_path, step
+  end
+
+  desc 'Pushes the schema to the next version (specify steps w/ STEP=n)'
+  task :forward => :environment do
+    set_keyspace
+    step = ENV['STEP'] ? ENV['STEP'].to_i : 1
+    ActiveColumn::Migrator.forward ActiveColumn::Migrator.migrations_path, step
+  end
+
+  private
+
+  def load_config
+    YAML.load_file(Rails.root.join("config", "cassandra.yml"))
+  end
+
+  def set_keyspace
+    config = load_config[Rails.env || 'development']
+    ActiveColumn::Tasks::Keyspace.new.set config[:keyspace]
+  end
+
 end
 
-def load_config
-  YAML.load_file(Rails.root.join("config", "cassandra.yml"))
-end
