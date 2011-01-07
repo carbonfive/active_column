60b13e3de68ce3a62948f776e79ab172bce3d039
Michael Wynholds <mike@carbonfive.com>
2011-01-06 18:12:50 -0800

    Add output to ks:migrate and other migration tasks.

--- lib/active_column/migration.rb
+++ lib/active_column/migration.rb
@@ -29,13 +29,30 @@ module ActiveColumn
 
   class Migration
 
+    @@verbose = true
+    cattr_accessor :verbose
+
     def self.connection
       $cassandra
     end
 
     def self.migrate(direction)
       return unless respond_to?(direction)
-      send direction
+
+      case direction
+        when :up   then announce "migrating"
+        when :down then announce "reverting"
+      end
+
+      result = nil
+      time = Benchmark.measure { result = send("#{direction}") }
+
+      case direction
+        when :up   then announce "migrated (%.4fs)" % time.real; write
+        when :down then announce "reverted (%.4fs)" % time.real; write
+      end
+
+      result
     end
 
     def self.create_column_family(name, options = {})
@@ -44,6 +61,38 @@ module ActiveColumn
 
     def self.drop_column_family(name)
       ActiveColumn::Tasks::ColumnFamily.new.drop(name)
+    end
+
+    def self.write(text="")
+      puts(text) if verbose
+    end
+
+    def self.announce(message)
+      version = defined?(@version) ? @version : nil
+
+      text = "#{version} #{name}: #{message}"
+      length = [0, 75 - text.length].max
+      write "== %s %s" % [text, "=" * length]
+    end
+
+    def self.say(message, subitem=false)
+      write "#{subitem ? "   ->" : "--"} #{message}"
+    end
+
+    def self.say_with_time(message)
+      say(message)
+      result = nil
+      time = Benchmark.measure { result = yield }
+      say "%.4fs" % time.real, :subitem
+      say("#{result} rows", :subitem) if result.is_a?(Integer)
+      result
+    end
+
+    def self.suppress_messages
+      save, self.verbose = verbose, false
+      yield
+    ensure
+      self.verbose = save
     end
 
   end
