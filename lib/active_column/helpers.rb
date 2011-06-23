d8df20fbfb1606eaacac6be09013df4f834247b5
Krieghan Riley <kriley@ngmoco.com>
2011-06-23 09:06:34 -0700

    removed a monkey-patch of Object.to_json.

--- lib/active_column/helpers.rb
+++ lib/active_column/helpers.rb
@@ -21,16 +21,16 @@ module ActiveColumn
 
   end
 end
-
-class Object
-  def to_json(*a)
-    result = {
-      JSON.create_id => self.class.name
-    }
-    instance_variables.inject(result) do |r, name|
-      r[name[1..-1]] = instance_variable_get name
-      r
-    end
-    result.to_json(*a)
-  end
-end
+#
+#class Object
+#  def to_json(*a)
+#    result = {
+#      JSON.create_id => self.class.name
+#    }
+#    instance_variables.inject(result) do |r, name|
+#      r[name[1..-1]] = instance_variable_get name
+#      r
+#    end
+#    result.to_json(*a)
+#  end
+#end
